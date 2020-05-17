cf10e027b59955bfd4a9e455f2553a72cef6bf4d
youjustgotpauled <55904704+youjustgotpauled@users.noreply.github.com>
2020-05-17 16:05:41 +0530

    Update gr_pr.py

--- gr_pr.py
+++ gr_pr.py
@@ -23,12 +23,7 @@ class TrafficSignNet:
         
         model.add(Conv2D(32, (3, 3), padding="same", activation = 'relu'))
         model.add(MaxPooling2D(pool_size=(2, 2),strides=2))
-        
-        model.add(Conv2D(32, (3, 3), padding="same", activation = 'relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2),strides=2))
-        
-        model.add(Conv2D(32, (3, 3), padding="same", activation = 'relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2),strides=2))
+      
         
         
      
@@ -36,9 +31,9 @@ class TrafficSignNet:
         model.add(Dense(128, activation = 'relu'))
         model.add(Dense(128, activation = 'relu'))
         #model.add(Dropout(rate = 0.1))
-        model.add(Dense(128, activation = 'relu',kernel_regularizer=keras.regularizers.l1_l2(l1=0.1,l2=0.01)))
         model.add(Dense(128, activation = 'relu'))
-        
+        model.add(Dense(128, activation = 'relu'))
+        #,kernel_regularizer=keras.regularizers.l1_l2(l1=0.1,l2=0.01
         
         model.add(Dense(classes, activation = 'softmax'))
         
